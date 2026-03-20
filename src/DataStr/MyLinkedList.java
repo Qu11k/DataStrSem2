4d279b10ea6e2d3d63caba84ed29472d0d47c2f0
Qu11k <filadelfija769@gmail.com>
2026-03-20 18:54:29 +0200

    added variables to new class

--- src/DataStr/MyLinkedList.java
+++ src/DataStr/MyLinkedList.java
@@ -1,5 +1,24 @@
 package DataStr;
 
 public class MyLinkedList {
+private myNode firstNode=null;
+private myNode lastNode=null;
+private int howManyElements=0;
+
+public int getHowManyElements() {
+	return howManyElements;
+}
+public boolean isEmpty() {
+	return (howManyElements==0);
+}
+public boolean isFUll() {
+	try{
+	myNode newNode =new myNode('a');
+	return false;
+	}
+	catch(OutOfMemoryError error)  {
+	return true;
+	}
+}
 
 }
--- src/DataStr/myNode.java
+++ src/DataStr/myNode.java
@@ -2,31 +2,39 @@ package DataStr;
 
 public class myNode {
 	private char element;
-	private myNode NextNode=null;
-	private myNode PreviousNode=null;
+	private myNode NextNode = null;
+	private myNode PreviousNode = null;
+
 	public char getElement() {
 		return element;
 	}
+
 	public void setElement(char element) {
 		this.element = element;
 	}
+
 	public myNode getNextNode() {
 		return NextNode;
 	}
+
 	public void setNextNode(myNode nextNode) {
 		NextNode = nextNode;
 	}
+
 	public myNode getPreviousNode() {
 		return PreviousNode;
 	}
+
 	public void setPreviousNode(myNode previousNode) {
 		PreviousNode = previousNode;
 	}
+
 	public myNode(char element) {
 		setElement(element);
 	}
+
 	public String toString() {
-		return""+element;
+		return "" + element;
 	}
 
 }
