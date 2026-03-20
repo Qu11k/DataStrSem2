6107eb54ea63e3a908d399ed11c0400a1cf19384
Qu11k <filadelfija769@gmail.com>
2026-03-20 18:33:33 +0200

    added variables getters and setters to class

--- src/DataStr/myNode.java
+++ src/DataStr/myNode.java
@@ -1,5 +1,26 @@
 package DataStr;
 
 public class myNode {
-
+	private char element;
+	private myNode NextNode=null;
+	private myNode PreviousNode=null;
+	public char getElement() {
+		return element;
+	}
+	public void setElement(char element) {
+		this.element = element;
+	}
+	public myNode getNextNode() {
+		return NextNode;
+	}
+	public void setNextNode(myNode nextNode) {
+		NextNode = nextNode;
+	}
+	public myNode getPreviousNode() {
+		return PreviousNode;
+	}
+	public void setPreviousNode(myNode previousNode) {
+		PreviousNode = previousNode;
+	}
+	
 }
